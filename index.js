f20e0667df7f9b1de165b188107d93cee1d0456a
souhaAmor <samor001@seattlecentral.edu>
2016-05-10 22:28:10 -0700

    create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+var task = require("./lib/task.js");
+
+
+
+// configure Express app
+app.set('port', process.env.PORT || 3000);
+app.use(express.static('public'));
+app.use(require("body-parser").urlencoded({extended: true}));
+
+// set template engine
+var handlebars = require('express-handlebars').create({defaultLayout: 'main', extname: '.hbs' });
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs' );
+
+var routes = require("./lib/routes")(app);
+
+app.use(function(req,res) {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started');    
+});
